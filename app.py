ce5d57cd8fa38aaa3a74ba087b4d9e74a20270d6
Yixiaohan <499065469@qq.com>
2014-12-21 14:00:42 +0800

    flask hello world

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'hello world'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
