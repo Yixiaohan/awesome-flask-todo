6d78ae1f24af565003ed60525701bb71e53f0a00
Yixiaohan <499065469@qq.com>
2014-12-21 15:19:42 +0800

    add form

--- app/views.py
+++ app/views.py
@@ -9,3 +9,12 @@ from app.models import Todo
 def index():
     todos = Todo.objects.all()
     return render_template("index.html", todos=todos)
+
+@app.route('/add', methods=['POST', ])
+def add():
+    content = request.form.get("content")
+    todo = Todo(content=content)
+    todo.save()
+
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
