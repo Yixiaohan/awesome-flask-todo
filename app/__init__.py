f21b7c95c17be39b719daf7303d781fe356ac1e9
Yixiaohan <499065469@qq.com>
2014-12-21 14:35:07 +0800

    创建项目的目录结构，使用Flask-Script 运行 Server

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object("config")
+
+from app import views, models
+
---
+++ app/views.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template("index.html", text="hello world")
