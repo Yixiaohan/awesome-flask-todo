75af4876dc2866ec2b00d48c4734856518a561c4
Yixiaohan <499065469@qq.com>
2014-12-21 15:11:23 +0800

    get all todos

--- app/views.py
+++ app/views.py
@@ -3,7 +3,9 @@
 
 from app import app
 from flask import render_template
+from app.models import Todo
 
 @app.route('/')
 def index():
-    return render_template("index.html", text="hello world")
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
