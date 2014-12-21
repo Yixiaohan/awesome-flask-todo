237efc542dc07e4166cd422057cc06547621380b
Yixiaohan <499065469@qq.com>
2014-12-21 16:18:30 +0800

    add form validate AND add CSRF KEY

--- app/views.py
+++ app/views.py
@@ -3,46 +3,54 @@
 
 from app import app
 from flask import render_template, request
-from app.models import Todo
+from app.models import Todo, TodoForm
+
 
 @app.route('/')
 def index():
+    form = TodoForm()
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
 
 @app.route('/add', methods=['POST', ])
 def add():
-    content = request.form.get("content")
-    todo = Todo(content=content)
-    todo.save()
+    form = TodoForm(request.form)
+    if form.validate():
+        content = request.form.get("content")
+        todo = Todo(content=content)
+        todo.save()
 
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
-
+    return render_template("index.html", todos=todos, form=form)
 
 @app.route('/done/<string:todo_id>')
 def done(todo_id):
+    form = TodoForm()
+
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.status = 1
     todo.save()
 
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
-
+    return render_template("index.html", todos=todos, form=form)
 
 @app.route('/undone/<string:todo_id>')
 def undone(todo_id):
+    form = TodoForm()
+
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.status = 0
     todo.save()
 
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
 
 @app.route('/delete/<string:todo_id>')
 def delete(todo_id):
+    form = TodoForm()
+
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.delete()
 
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+SECRET_KEY = "never tell you"
 MONGODB_SETTINGS = {'DB': 'todo_db'}
